89c5a66c892dd0ec2b882ac952a96bb2ec1aca88
Rm Hridoy <55066846+rmhridoy@users.noreply.github.com>
2019-10-06 17:51:55 +0600

    Add files via upload

---
+++ problem.php
@@ -0,0 +1,32 @@
+<?php 
+
+// Input from user .......
+$fistInput = 2;
+$secondInput = 5;
+
+// initialize variable
+$i = $fistInput;
+$n = $secondInput;
+
+
+// Make a for loop for sum
+
+for ($i; $i <= $n; $i++) { 
+   $sum = $sum + $i;
+}
+
+echo "Sumation is =".$sum;
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
